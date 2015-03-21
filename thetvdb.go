91d9d40899d9d22d68c9885fba42dbd224e2e307
Rocco Galluzzo <rocco@galluzzo.me>
2015-03-21 14:00:05 +0100

    Add request and XML methods

--- thetvdb.go
+++ thetvdb.go
@@ -8,18 +8,65 @@ import (
 
 type TheTVDB struct {
   apiKey    string
-  lang      string
+  language  string
 }
 
-const ApiURL = 'http://thetvdb.com/api'
+const ApiURL = 'http://thetvdb.com/api/'
+
+// GetSeries
+// params seriesname
+// params language
+const GetSeriesPath = 'GetSeries.php'
+
+// GetSeriesByRemoteID
+// params seriesname
+// params language
+// params imdbid
+// params language
+// params zap2it
+const GetSeriesByRemoteIDPath = 'GetSeriesByRemoteID.php'
+
+// GetEpisodeByAirDate
+// params apikey
+// params seriesid
+// params airdate
+// params language
+const GetEpisodeByAirDatePath = 'GetSeriesByRemoteID.php'
 
 var httpClient = &http.Client{Timeout: 60}
 
+func (t *TheTVDB) GetResourceURL(resource, params[string]string) (string, error) {
+  var Url = &url.URL
+  Url, error := url.Parse(ApiURL)
 
-func (t *TheTVDB) Call(path string) (*http.Request, error) {
+  switch resource {
+  case 'GetSeries':
+    Url.Path += GetSeriesPath
+  case 'GetSeriesByRemoteID':
+    Url.Path += GetSeriesByRemoteIDPath
+  case 'GetEpisodeByAirDate':
+    Url.Path += GetEpisodeByAirDatePath
+    parameters.Add('apikey', t.apikey)
+  }
+  parameters := url.Values{}
+  parameters.Add('language', t.language)
+  for k, v := range params {
+    parameters.Add(k, v)
+  }
 
+  Url.RawQuery = parameters.Encode()
+  return Url.String(), error
 }
 
-func (t *TheTVDB) ParseXML(req *http.Request) (response) {
+func (t *TheTVDB) Get(resource string, params[string]string) (interface{}, error) {
+  url, error := t.GetResourceURL(resource, params)
+  response, error := http.Get(url)
+  defer response.Body.Close()
+  body, error := ioutil.ReadAll(response.Body)
+  response, error = ParseXML(body)
+  return response, error
+}
 
+func (t *TheTVDB) UnmarshalXML(resource string, xml_response string) (interface{}, error) {
+  err := xml.Unmarshal([]byte(xml_response), &Series)
 }
